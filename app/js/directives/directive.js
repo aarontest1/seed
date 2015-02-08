15a6e75adb4adc9cdcba5ddd766768ce1d4d042f
aaron <aar0n_r@hotmail.com>
2015-02-08 22:06:37 +0000

    news

--- app/js/directives/directive.js
+++ app/js/directives/directive.js
@@ -1,1 +1,13 @@
 'use strict';
+
+myApp.directive('roundButton', function () {
+    return {
+        restrict: 'A',
+        replace: true,
+        transclude: true,
+        templateUrl: 'js/directives/templates/roundButton.html',
+        link: function (scope, element, attrs) {
+            // DOM manipulation/events here!
+        }
+    };
+});
--- server.js
+++ server.js
@@ -1,45 +1,38 @@
 var http = require('http');
+var url = require('url');
 var path = require('path');
 var fs = require('fs');
-
-//a helper function to handle HTTP requests
-function requestHandler(req, res) {
-    var content = '';
-    var fileName = path.basename(req.url);//the file that was requested
-    var localFolder = __dirname + '/app/';//where our public files are located
-
-    //NOTE: __dirname returns the root folder that this javascript file is in.
-
-    if(fileName === 'index.html'){//if index.html was requested...
-        content = localFolder + fileName;//setup the file name to be returned
-
-        console.log(content);
-
-        //reads the file referenced by 'content'
-        //and then calls the anonymous function we pass in
-        fs.readFile(content,function(err,contents){
-            //if the fileRead was successful...
-            if(!err){
-                //send the contents of index.html
-                //and then close the request
-                res.end(contents);
-            } else {
-                //otherwise, let us inspect the eror
-                //in the console
-                console.dir(err);
-            };
-        });
-    } else {
-        //if the file was not found, set a 404 header...
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        //send a custom 'file not found' message
-        //and then close the request
-        res.end('<h1>Sorry, the page you are looking for cannot be found.</h1>');
-    };
+var mimeTypes = {
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css"
 };
 
-//step 2) create the server
-http.createServer(requestHandler)
+http.createServer(function(req, res) {
+    var uri = url.parse(req.url).pathname;
+    var filename = path.join(process.cwd(), uri);
+    path.exists(filename, function(exists) {
+        if(!exists) {
+            console.log("bad request: " + filename);
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write('404 Not Found\n');
+            res.end();
+            return;
+        }
+        var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
+        res.writeHead(200, {'Content-Type':mimeType});
 
-//step 3) listen for an HTTP request on port 3000
-    .listen(3000);
+        var fileStream = fs.createReadStream(filename);
+        fileStream.pipe(res);
+        //fileStream.on('data', function (data) {
+        //    res.write(data);
+        //});
+        //fileStream.on('end', function() {
+        //    res.end();
+        //});
+
+    });
+}).listen(1337);
