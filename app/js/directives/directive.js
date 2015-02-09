ac3c66a44f39fc2e1fb2cc582f77b7e227a1f845
aaron <aar0n_r@hotmail.com>
2015-02-09 00:08:09 +0000

    more

--- app/js/directives/directive.js
+++ app/js/directives/directive.js
@@ -4,7 +4,6 @@ myApp.directive('roundButton', function () {
     return {
         restrict: 'A',
         replace: true,
-        transclude: true,
         templateUrl: 'js/directives/templates/roundButton.html',
         link: function (scope, element, attrs) {
             // DOM manipulation/events here!
